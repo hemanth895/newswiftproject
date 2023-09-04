3830318c72b96fff1891027b33bb1755e58e5e28
hemanth <hemanth.reddy@accreteglobus.com>
2023-09-04 16:59:10 +0530

    made changes one and only file

--- hegfrhegswf/ContentView.swift
+++ hegfrhegswf/ContentView.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //ertwer
+    //weregrtwertwert
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, worewuytryuew3tryteqwld!")
         }
         .padding()
     }
