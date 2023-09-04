c52994cb53cd5eec94c62679fcc8baf2559bd486
hemanth <hemanth.reddy@accreteglobus.com>
2023-09-04 16:55:37 +0530

    Initial Commit

---
+++ hegfrhegswf/hegfrhegswfApp.swift
@@ -0,0 +1,17 @@
+//
+//  hegfrhegswfApp.swift
+//  hegfrhegswf
+//
+//  Created by Accrete - Mac 2 on 04/09/23.
+//
+
+import SwiftUI
+
+@main
+struct hegfrhegswfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
